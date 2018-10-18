cb6d3f11708366cb16f253b7a43888f39d4f774a
Jannis Klinkenberg <j.klinkenberg@itc.rwth-aachen.de>
2018-10-18 09:09:15 +0200

    added sparse CG code

---
+++ sparse_CG/tasking/fast_run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+PROG_CMD="./solver.exe ../DATA/mat235M.mtxdump"
+#PROG_VERSION=deb
+PROG_VERSION=rel
+
+export KMP_TASK_STEALING_CONSTRAINT=0
+export OMP_PLACES=cores 
+export OMP_PROC_BIND=spread 
+export OMP_NUM_THREADS=12
+
+# additionally specify number of tasks / over-decomposition
+export CG_NUM_TASKS=$(($OMP_NUM_THREADS * 16))
+export PAR_PRODUCER=1
+
+function eval_run {
+  curname=$1
+  if ! [ -z "$2" ]; then
+    curname="${curname}.$2"
+  fi
+  echo "Executing affinity ${curname}"
+  make ${PROG_VERSION}."$1"
+
+  # make sure to disable NUMA balancer (if available on your system)
+  # we had a script for that on our systems called "no_numa_balancing"
+  # no_numa_balancing "${PROG_CMD}" &> output_${curname}.txt
+  "${PROG_CMD}" &> output_${curname}.txt
+
+  grep "Elapsed time" output_${curname}.txt
+}
+
+# clean first
+make clean
+
+# make sure custom llvm library will be executed (set explicitly or use module system)
+export LD_LIBRARY_PATH=${RUNTIME_AFF_REL_DIR}/lib:${LD_LIBRARY_PATH}
+export LIBRARY_PATH=${RUNTIME_AFF_REL_DIR}/lib:${LIBRARY_PATH}
+
+export INCLUDE=${RUNTIME_AFF_REL_DIR}/include:${INCLUDE}
+export CPATH=${RUNTIME_AFF_REL_DIR}/include:${CPATH}
+export C_INCLUDE_PATH=${RUNTIME_AFF_REL_DIR}/include:${C_INCLUDE_PATH}
+export CPLUS_INCLUDE_PATH=${RUNTIME_AFF_REL_DIR}/include:${CPLUS_INCLUDE_PATH}
+
+# execute tests with and without affinity
+eval_run "llvm"
+eval_run "domain.lowest"
+#eval_run "domain.rand"
+#eval_run "domain.round_robin"
+eval_run "thread.lowest"
+#eval_run "thread.rand"
+#eval_run "thread.round_robin"
+
+
+
